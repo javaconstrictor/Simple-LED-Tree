bf1efe5decf180dca39db99daa39e9f659eaf604
Logan Miller <101066393+javaconstrictor@users.noreply.github.com>
2023-04-27 03:23:16 -0600

    V1.0
    this was a poorly optimized and quickly done project to make a last minute Christmas gift for a family friend. I may revisit this project and make some changes for better gui control, better performance, and more options, as well as sliders to customize maximum brightness. for now it is archived.

--- Sketch_WiFi_tree_light.ino
+++ Sketch_WiFi_tree_light.ino
@@ -1,16 +1,16 @@
 #include <ESP8266WiFi.h>
 
 WiFiServer Server(80);
-const char* ssid = "ssid";
+const char* ssid = "ssid";                  //not sure why i decided to use character variable?
 const char* password = "pW";
 
 int g = 5;                               //initializing led control pins and 
 int b = 4;                              // including wifi library
 int r = 0;
-int brightness = 0;
+int brightness = 0;                     //setting a timer and maths for wave effect
 int revbright = 255-brightness;
 int fade = 5;
-int clickCounter = 0;
+int clickCounter = 0;                   //this is for cycling through modes
 
 void setup() {
   pinMode(g,OUTPUT);                    // set pins to output, test all
@@ -21,7 +21,7 @@ void setup() {
   digitalWrite(b,LOW);
 
   Serial.begin (74880);
-  Serial.print ("welcome!");                      //welcome matt for serial mon
+  Serial.print ("welcome!");                      //welcome mat for serial monitor
   Serial.println ();
   Serial.print ("initializing, please hold.");
   Serial.println ();
@@ -35,10 +35,10 @@ void setup() {
 
         while(WiFi.status() != WL_CONNECTED ) {
               delay (250);
-              Serial.print (".");
+              Serial.print (".");                           //shows the device is still working and has not frozen
         }
     
-    digitalWrite (g,HIGH);
+    digitalWrite (g,HIGH);                                  //end LED test, also shows that device is on and ready for client connection.
     digitalWrite (r,HIGH);
     digitalWrite (b,HIGH);
     Serial.println ();
@@ -48,7 +48,7 @@ void setup() {
 
   Server.begin();
   Serial.println ();
-  Serial.print ("Setup complete! please test client site");
+  Serial.print ("Setup complete! please connect to client site");
 }
 
 void loop() {
@@ -75,11 +75,13 @@ void loop() {
           } else if (c != '\r') {
             currentLine += c;
           }
-
+          
           if (currentLine.endsWith("GET /H")) { clickCounter++;
-        }
+                                                }
+          if (clickCounter == 10 { clickCounter = 0}
+              
       }
-      if (clickCounter == 1) {
+      if (clickCounter == 1) {                    //these are the lighting modes, written in PWM. mode 0 is off, 1 changes color with no fade
         analogWrite (r, 200);
         delay (1500);
         analogWrite (g, 200);
@@ -93,7 +95,7 @@ void loop() {
         analogWrite (g, 0);
         analogWrite (b, 0);
       } else if (clickCounter == 2) {
-        digitalWrite (r,LOW);
+        digitalWrite (r,LOW);                       //just red
         digitalWrite (g,LOW);
         digitalWrite (b,LOW);
         delay(1000);
@@ -101,14 +103,14 @@ void loop() {
         delay(1000);
       } else if (clickCounter == 3) {
         digitalWrite (r,LOW);
-        digitalWrite (g,LOW);
+        digitalWrite (g,LOW);                         // just green
         digitalWrite (b,LOW);
         delay(1000);
         digitalWrite (g,HIGH);
         delay(1000);
       } else if (clickCounter == 4) {
         digitalWrite (r,LOW);
-        digitalWrite (g,LOW);
+        digitalWrite (g,LOW);                           //just blue
         digitalWrite (b,LOW);
         delay(1000);
         digitalWrite (b,HIGH);
@@ -117,7 +119,7 @@ void loop() {
         digitalWrite (r,LOW);
         digitalWrite (b,LOW);
         digitalWrite (g,HIGH);
-        delay(1000);
+        delay(1000);                                    // red and green no fade
         digitalWrite (r,HIGH);
         digitalWrite (g,LOW);
         delay(1000);
@@ -126,26 +128,26 @@ void loop() {
         
       } else if (clickCounter == 6) {
         analogWrite (r,brightness);
-        brightness = brightness+fade;
+        brightness = brightness+fade;                       //red fade
         if (brightness == 0 || brightness == 255) {
           fade = -fade;
         }
       }else if (clickCounter == 7) {
         analogWrite (g,brightness);
         brightness = brightness+fade;
-        if (brightness == 0 || brightness == 255) {
+        if (brightness == 0 || brightness == 255) {           //green fade
           fade = -fade;
         }
      }else if (clickCounter == 8) {
         analogWrite (b,brightness);
         brightness = brightness+fade;
-        if (brightness == 0 || brightness == 255) {
+        if (brightness == 0 || brightness == 255) {             //blue fade
           fade = -fade;
         }
         }else if (clickCounter == 9) {
         analogWrite (g,brightness);
         analogWrite (r,revbright);
-        brightness = brightness+fade;
+        brightness = brightness+fade;                             //red and green fading against eachother
         if (brightness == 0 || brightness == 255) {
           fade = -fade; 
     }
